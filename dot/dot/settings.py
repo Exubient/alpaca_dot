56ec620ea00337ce9ff135a480d9d84da12d8609
Exubient <henrynicholas09@gmail.com>
2017-08-31 17:37:18 +0900

    hello world

--- dot/dot/settings.py
+++ dot/dot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'root', 
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -116,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static/')
 STATIC_URL = '/static/'
---
+++ dot/root/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def root(request):
+	if request.method =="GET":
+		return render(request, 'root/src/etc/test.html', {})
+	elif request.method == "POST":
+		startAdmin()
+		return render(request, 'response_time/src/etc/root.html', {})
+
