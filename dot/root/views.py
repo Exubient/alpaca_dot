10c2b1b6de6306d30edeba43df08e49058350f68
Exubient <henrynicholas09@gmail.com>
2017-09-05 16:56:58 +0900

    delete untouched

--- dot/root/views.py
+++ dot/root/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def root(request):
-	if request.method =="GET":
-		return render(request, 'root/src/index.html', {})
-	elif request.method == "POST":
-		startAdmin()
-		return render(request, 'response_time/src/etc/root.html', {})
+	return render(request, 'root/src/landing/index.html', {})
 
