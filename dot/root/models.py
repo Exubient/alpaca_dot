8a357ce28f683e7282509d554d75db7b13b6dfaf
류다윤 <ekdbs3675@naver.com>
2017-09-12 22:01:31 +0900

    models.py 3번째 수정, Post와 Member 관계가 1:1 관계로해놨는데 이걸 다대다로 바꿔야함

--- dot/root/models.py
+++ dot/root/models.py
@@ -2,31 +2,33 @@ import re
 from django.db import models
 from django.forms import ValidationError
 
-TYPE_CHOICES = (
+
+
+class Member(models.Model):
+    TYPE_CHOICES = (
     ('o','organization'),
     ('v','volunteer'),
 )
-
-class Member(models.Model):
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=30)
-    phone = models.IntegerField(max_length=12)
+    phone = models.IntegerField()
     email = models.EmailField()
-    m_Type = models.ChoiceField(choices = TYPE_CHOICES)
+    m_type = models.CharField(max_length=1, choices = TYPE_CHOICES)
+    Post_set = models.ManyToManyField('Post')
     matching_set = models.ManyToManyField('Matching')
 
 class Post(models.Model):
-    org = models.ForeignKey(Member)
+    org = models.ForeignKey(Member,unique=True)
     title = models.CharField(max_length=20)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
 class Matching(models.Model):
-    vol = models.ForeignKey(Post,to_field="org_id")
-    org = models.ForeignKey(Member) # 이렇게 하는게 맞나싶음..
-    post = models.ForeignKey(Post) # 어떤 포스트에대해서 무슨관계인지 알기위해선 추가하는게 좋을듯
-    state = models.CharField()
+    vol = models.ForeignKey(Member,related_name='Machng_vol')
+    org = models.ForeignKey(Post, to_field="org_id", related_name='Maching_org') # 이렇게 하는게 맞나싶음..
+    post = models.ForeignKey(Post,) # 어떤 포스트에대해서 무슨관계인지 알기위해선 추가하는게 좋을듯
+    state = models.CharField(max_length=20)
 
 
 class Comment(models.Model):
