478c40cfabcf683ef6be8d2e18226c173e92e82b
류다윤 <ekdbs3675@naver.com>
2017-09-12 18:22:09 +0900

    다윤 models.py 2차 수정

--- dot/root/models.py
+++ dot/root/models.py
@@ -12,25 +12,26 @@ class Member(models.Model):
     location = models.CharField(max_length=30)
     phone = models.IntegerField(max_length=12)
     email = models.EmailField()
-   # Type = models.ChoiceField(choices = TYPE_CHOICES) #type은 예약어같음
+    m_Type = models.ChoiceField(choices = TYPE_CHOICES)
+    matching_set = models.ManyToManyField('Matching')
 
 class Post(models.Model):
-    org_id = models.ForeignKey(Member)
+    org = models.ForeignKey(Member)
     title = models.CharField(max_length=20)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
 class Matching(models.Model):
-    vol_id = models.ForeignKey(Post,to_field="org_id")
-    org_id = models.ForeignKey(Member) # 이렇게 하는게 맞나싶음..
+    vol = models.ForeignKey(Post,to_field="org_id")
+    org = models.ForeignKey(Member) # 이렇게 하는게 맞나싶음..
+    post = models.ForeignKey(Post) # 어떤 포스트에대해서 무슨관계인지 알기위해선 추가하는게 좋을듯
     state = models.CharField()
 
 
 class Comment(models.Model):
-    post_id = models.ForeignKey(Post)
-    vol_id = models.ForeignKey(Member)
-    org_id = models.ForeignKey(Member)
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(Member) #org와 vol을 굳이 나눌 필요 없어보임.
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
